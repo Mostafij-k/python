ce86f46ace30f9f0a70b332326715e1adfc6ee1e
Microbiologist <mostafij306@gmail.com>
2021-11-25 22:08:03 +0600

    New code

--- programe.py
+++ programe.py
@@ -1,2 +1,14 @@
 print(5+6)
 print(8+7)
+#Input phase
+nub1 = int(input('Input 1st number: '))
+nub2 = int(input('Input 2nd number: '))
+#Processing Phase 
+result = nub1+nub2
+#Output
+print(result)
+
+
+
+
+
